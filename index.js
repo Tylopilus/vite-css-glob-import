2969b8b4eb7b809ce2b5af5e099bd7ae8f452562
Helge <elitaer@gmail.com>
2022-12-07 09:56:59 +0100

    Fix bug where files can be imported with _

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import fs from 'fs';
-import path from 'path';
-import FastGlob from 'fast-glob';
+const fs = require('fs');
+const path = require('path');
+const FastGlob = require('fast-glob');
 
-export default function myPlugin() {
+function myPlugin() {
   return {
     name: 'vite-css-glob-import', // required, will show up in warnings and errors
 
@@ -10,30 +10,49 @@ export default function myPlugin() {
       if (id.endsWith('.scss')) {
         // console.log({ id });
         // read file
-        const file = fs.readFileSync(id, 'utf-8');
-        // console.log(file);
+        let file = fs.readFileSync(id, 'utf-8');
+        console.log(file);
         // read file line by line to find all statements that start with @import and contain a glob pattern
         const lines = file.split('\n');
         const importLines = lines.filter((line) => line.startsWith('@import'));
 
-        const globPatterns = importLines.map((line) => {
+        const toReplace = importLines.map((line) => {
           const globPattern = line.split(' ')[1];
           const res = globPattern.split("'")[1];
 
-          return path.resolve(path.dirname(id), res);
+          let importFile = res.split('/').at(-1);
+          if (!importFile?.endsWith('.scss')) {
+            importFile = `_${importFile}.scss`;
+          }
+          const fileToImport = path.join(
+            res.split('/').slice(0, -1).join('/'),
+            importFile
+          );
+          // console.log({ fileToImport });
+
+          const files = FastGlob.sync(
+            path.resolve(path.dirname(id), fileToImport)
+          );
+          console.log('here', files);
+          return [line, files.map((file) => `@import "${file}";`).join('\n')];
         });
 
-        // console.log({ globPatterns });
-
-        const files = FastGlob.sync([...globPatterns]);
-        console.log({ files });
-        const res = files
-          .filter((file) => file !== id)
-          .map((file) => `@import "${file}";`)
-          .join('\n');
-        console.log({ res });
-        return res;
+        // console.log({ toReplace });
+        for (const [search, replace] of toReplace) {
+          file = file.replace(search, replace);
+        }
+        console.log(file);
+        return file;
+        // const files = FastGlob.sync([...globPatterns]);
+        // console.log({ files });
+        // const res = files
+        //   .filter((file) => file !== id)
+        //   .map((file) => `@import "${file}";`)
+        //   .join('\n');
+        // console.log({ res });
+        // return res;
       }
     },
   };
 }
+module.exports = myPlugin;
