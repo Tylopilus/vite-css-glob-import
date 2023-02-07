dc619b901e1f79d0c81a46f10547966f635be42f
Helge <elitaer@gmail.com>
2023-02-07 16:58:39 +0100

    fix issue w/ import of index files

--- index.js
+++ index.js
@@ -15,8 +15,40 @@ function myPlugin() {
         // read file line by line to find all statements that start with @import and contain a glob pattern
         const lines = file.split('\n');
         const importLines = lines.filter((line) => line.startsWith('@import'));
-
+        const useLines = lines.filter((line) => line.startsWith('@use'));
         const toReplace = importLines.map((line) => {
+          const globPattern = line.split(' ')[1];
+          const res = globPattern.split("'")[1];
+
+          let importFile = res.split('/').at(-1);
+          if (!importFile?.endsWith('.scss')) {
+            // check if importFile is a directory
+            if (fs.existsSync(path.join(path.dirname(id), res))) {
+              const tmpName = `${importFile}/index.scss`;
+              // check if tmpName exists
+              if (fs.existsSync(path.join(path.dirname(id), tmpName))) {
+                importFile = tmpName;
+              } else {
+                importFile = `${importFile}/_index.scss`;
+              }
+            } else {
+              importFile = `_${importFile}.scss`;
+            }
+          }
+          const fileToImport = path.join(
+            res.split('/').slice(0, -1).join('/'),
+            importFile
+          );
+          // console.log({ fileToImport });
+
+          const files = FastGlob.sync(
+            path.resolve(path.dirname(id), fileToImport)
+          );
+          // console.log('here', files);
+          return [line, files.map((file) => `@import "${file}";`).join('\n')];
+        });
+
+        const toReplaceUse = useLines.map((line) => {
           const globPattern = line.split(' ')[1];
           const res = globPattern.split("'")[1];
 
@@ -34,11 +66,13 @@ function myPlugin() {
             path.resolve(path.dirname(id), fileToImport)
           );
           // console.log('here', files);
-          return [line, files.map((file) => `@import "${file}";`).join('\n')];
+          return [line, files.map((file) => `@use "${file}";`).join('\n')];
         });
-
         // console.log({ toReplace });
         for (const [search, replace] of toReplace) {
+          file = file.replace(search, replace);
+        }
+        for (const [search, replace] of toReplaceUse) {
           file = file.replace(search, replace);
         }
         // console.log(file);
